83e82174863bbab6789e5a97a4321bba9e196bbe
Титан <fignin@ya.ru>
2020-08-03 16:43:02 +0500

    Update Source.cpp
    исправлены русские символы, удалены строчки кода для дебага

--- Source.cpp
+++ Source.cpp
@@ -10,26 +10,27 @@ using namespace std;
 #define ushort unsigned short
 #define byte unsigned char
 
-enum { черн, тсин, тзел, тгол, крас, лил, жел, ссер, сер, син, зел, гол, роз, фиол, тел, бел };
-char default_background = 	черн,
-	default_text_color = 	бел,
-	input_color = 			жел,
-	negative_color = 		крас,
-	positive_color = 		зел,
-	progress_color =		гол;
-		
+enum { С‡РµСЂРЅ, С‚СЃРёРЅ, С‚Р·РµР», С‚РіРѕР», РєСЂР°СЃ, Р»РёР», Р¶РµР», СЃСЃРµСЂ, СЃРµСЂ, СЃРёРЅ, Р·РµР», РіРѕР», СЂРѕР·, С„РёРѕР», С‚РµР», Р±РµР» };
+char default_background = С‡РµСЂРЅ,
+default_text_color = Р±РµР»,
+input_color = Р¶РµР»,
+negative_color = РєСЂР°СЃ,
+positive_color = Р·РµР»,
+progress_color = РіРѕР»;
 
-HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); //Для setconsole (изм цвета фона и текста)
+
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); //Р”Р»СЏ setconsole (РёР·Рј С†РІРµС‚Р° С„РѕРЅР° Рё С‚РµРєСЃС‚Р°)
 void clr(char background, char color) { SetConsoleTextAttribute(hConsole, (WORD)((background << 4) | color)); }
 void clr(char color) { SetConsoleTextAttribute(hConsole, (WORD)((default_background << 4) | color)); }
 void clr() { SetConsoleTextAttribute(hConsole, (WORD)((default_background << 4) | default_text_color)); }
 
-bool qu() { //Функция вопроса
+bool qu() { //Р¤СѓРЅРєС†РёСЏ РІРѕРїСЂРѕСЃР°
 	char* q = new char[31]; bool answer;
+	printf(" ");
 	clr(input_color);
 	do { fgets(q, 31, stdin); } while (q[0] == '\n');
 	clr();
-	if (((q[0] == 'д') || (q[0] == 'y') || (q[0] == '1') || (strstr(q, "конечно") != 0) || (strstr(q, "positive") != 0) || (strstr(q, "Sbrakets") != 0) || (strstr(q, "ок") != 0) || (strstr(q, "ладно") != 0) || (strstr(q, "угу") != 0) || (strstr(q, "ага") != 0) || (strstr(q, "хорошо") != 0) || (strstr(q, "хочу") != 0)) && (q[3] != 'н')) answer = true; else answer = false;
+	if (((q[0] == 'Рґ') || (q[0] == 'y') || (q[0] == '1') || (strstr(q, "РєРѕРЅРµС‡РЅРѕ") != 0) || (strstr(q, "positive") != 0) || (strstr(q, "Sbrakets") != 0) || (strstr(q, "РѕРє") != 0) || (strstr(q, "Р»Р°РґРЅРѕ") != 0) || (strstr(q, "СѓРіСѓ") != 0) || (strstr(q, "Р°РіР°") != 0) || (strstr(q, "С…РѕСЂРѕС€Рѕ") != 0) || (strstr(q, "С…РѕС‡Сѓ") != 0)) && (q[3] != 'РЅ')) answer = true; else answer = false;
 	delete[] q;
 	return answer;
 }
@@ -45,10 +46,10 @@ unsigned int getuint() {
 	for (unsigned char i = 0;;)
 	{
 		i = _getch();
-		if (i == '\r') { break; } //если Enter
+		if (i == '\r') { break; } //РµСЃР»Рё Enter
 		if (Isdigit(i)) { val = val * 10 + (i - 48); printf("%c", i); }
-		if ((i == '\b')) { //Если нажат backspace
-			if (val != 0) { val = floor(val / 10); printf("\b \b"); } //Если есть число
+		if ((i == '\b')) { //Р•СЃР»Рё РЅР°Р¶Р°С‚ backspace
+			if (val != 0) { val = floor(val / 10); printf("\b \b"); } //Р•СЃР»Рё РµСЃС‚СЊ С‡РёСЃР»Рѕ
 		}
 	}
 	clr();
@@ -65,27 +66,27 @@ int getint() {
 		if (i == '\r') { if (minus) val *= -1; break; }
 		if (i == '-' && !minus) { printf("%c", i); minus = true; }
 		if (Isdigit(i)) { val = val * 10 + (i - 48); printf("%c", i); }
-		if ((i == '\b')) { //Если нажат backspace
-			if (val != 0) { val = floor(val / 10); printf("\b \b"); } //Если есть число
+		if ((i == '\b')) { //Р•СЃР»Рё РЅР°Р¶Р°С‚ backspace
+			if (val != 0) { val = floor(val / 10); printf("\b \b"); } //Р•СЃР»Рё РµСЃС‚СЊ С‡РёСЃР»Рѕ
 			else if (minus) { minus = false; printf("\b \b"); }
-		} //Если есть минус
+		} //Р•СЃР»Рё РµСЃС‚СЊ РјРёРЅСѓСЃ
 	}
 	clr();
 	return val;
 }
 
 void perr(string massage) {
-	clr(negative_color); cout << "\n" << massage<< (char)(7); clr();
+	clr(negative_color); cout << "\n" << massage << (char)(7); clr();
 }
 
-string getstring() { //Нормальная функция ввода строки, исключающая возможность ввести пустую строку + цвет
+string getstring() { //РќРѕСЂРјР°Р»СЊРЅР°СЏ С„СѓРЅРєС†РёСЏ РІРІРѕРґР° СЃС‚СЂРѕРєРё, РёСЃРєР»СЋС‡Р°СЋС‰Р°СЏ РІРѕР·РјРѕР¶РЅРѕСЃС‚СЊ РІРІРµСЃС‚Рё РїСѓСЃС‚СѓСЋ СЃС‚СЂРѕРєСѓ + С†РІРµС‚
 	string input = "";
 	clr(input_color);
 	char symbol;
 	while (true) {
 		symbol = _getch();
 
-		if (symbol == '\r') if (input == "") continue; else break; //Завершить ввод при нажатии Enter только ЕСЛИ строка не пуста
+		if (symbol == '\r') if (input == "") continue; else break; //Р—Р°РІРµСЂС€РёС‚СЊ РІРІРѕРґ РїСЂРё РЅР°Р¶Р°С‚РёРё Enter С‚РѕР»СЊРєРѕ Р•РЎР›Р СЃС‚СЂРѕРєР° РЅРµ РїСѓСЃС‚Р°
 		if (symbol == '\b') if (input != "") { printf("\b \b"); input.pop_back(); continue; }
 		else continue;
 
@@ -102,26 +103,26 @@ void main() {
 	bool Repeat = true;
 	while (Repeat) {
 		clr();
-		printf("Выбрать путь по умолчанию?");
+		printf("Р’С‹Р±СЂР°С‚СЊ РїСѓС‚СЊ РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ?");
 		string FilePath;
 		if (qu()) FilePath = "C:\\css.txt";
 		else
 		{
-			printf("Введите путь: ");
+			printf("Р’РІРµРґРёС‚Рµ РїСѓС‚СЊ: ");
 			clr(input_color); string FilePath = getstring();
 		}
-		printf("Выбрать текст по умолчанию?");
+		printf("Р’С‹Р±СЂР°С‚СЊ С‚РµРєСЃС‚ РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ?");
 		string AddText;
 		if (qu()) AddText = "[dir] ";
 		else {
-			printf("Введите текст: ");
+			printf("Р’РІРµРґРёС‚Рµ С‚РµРєСЃС‚: ");
 			AddText = getstring();
 		}
 
 		ifstream rd;
 		rd.open(FilePath);
 		while (!rd) {
-			perr("Не удалось открыть файл"); printf(", повторить?");
+			perr("РќРµ СѓРґР°Р»РѕСЃСЊ РѕС‚РєСЂС‹С‚СЊ С„Р°Р№Р»"); printf(", РїРѕРІС‚РѕСЂРёС‚СЊ?");
 			if (qu()) { rd.close(); rd.open(FilePath); }
 			else break;
 		}
@@ -132,88 +133,80 @@ void main() {
 			char symbol; bool firstBlock = true;
 			do {
 				symbol = rd.get();
-				if (symbol == 13 || symbol == ','||symbol == '\n') { // \n
+				if (symbol == 13 || symbol == ',' || symbol == '\n') { // \n
 					wr << symbol;
 					while (true) {
 						symbol = rd.get();
 						if (symbol == ' ') wr << symbol;
 						else break;
 					}
-					if (symbol == '.'|| symbol == '#') {
+					if (symbol == '.' || symbol == '#') {
 						wr << AddText;
 					}
 				}
-				
-					if (symbol == '/') { //Комментарии
-						wr << symbol;
-						symbol = rd.get();
-						wr << symbol;
-						if (symbol == '/') {// "//"
-							do {
-								symbol = rd.get();
-								wr << symbol;
-							} while (symbol != '\n'); //Переписывать, пока строка не закончится
-						}
-						else if (symbol == '*') {// "/*"
-							while (true) {
+
+				if (symbol == '/') { //РљРѕРјРјРµРЅС‚Р°СЂРёРё
+					wr << symbol;
+					symbol = rd.get();
+					wr << symbol;
+					if (symbol == '/') {// "//"
+						do {
+							symbol = rd.get();
+							wr << symbol;
+						} while (symbol != '\n'); //РџРµСЂРµРїРёСЃС‹РІР°С‚СЊ, РїРѕРєР° СЃС‚СЂРѕРєР° РЅРµ Р·Р°РєРѕРЅС‡РёС‚СЃСЏ
+					}
+					else if (symbol == '*') {// "/*"
+						while (true) {
+							symbol = rd.get(); wr << symbol;
+							if (symbol == '*') {
 								symbol = rd.get(); wr << symbol;
-								int cur = rd.tellg();
-								if (symbol == '\n') {
-									printf("");
-								}
-								if (symbol == '*') {
-									symbol = rd.get(); wr << symbol;
-									if (symbol == '/') break; // читать, пока комментарий не закончится ("*/")
-								}
+								if (symbol == '/') break; // С‡РёС‚Р°С‚СЊ, РїРѕРєР° РєРѕРјРјРµРЅС‚Р°СЂРёР№ РЅРµ Р·Р°РєРѕРЅС‡РёС‚СЃСЏ ("*/")
 							}
 						}
 					}
-					else
-						if (symbol == '{') { //блоки
-							wr << symbol;
-							do {
-								symbol = rd.get();
-								wr << symbol;
-							} while (symbol != '}');  //читать, пока блок не закончится
-						}
-						else
-							if (symbol == '@')
-							{
-								wr << symbol;
-								string input = "";
-								for (int i = 0; i < strlen("-moz-document"); i++)
-								{
-									input += rd.get();
-									if (input[i] == '/' || input[i] == '*' || input[i] == '{' || input[i] == ',' || input[i] == 13) {
-										rd.seekg(-i, ios::cur);
-										break;
-									}
-								}
-								if (input == "-moz-document") {
-									wr << input;
-									do {
-										symbol = rd.get();
-										wr << symbol;
-									} while (symbol != '{'); //После -moz-document игнорируется открытие блока
-								}
-								else {
-									perr("Внимание: @ обнаружен, ожидалось "); clr(progress_color); printf("-moz-document");
-									clr(negative_color); printf("а обнаружено "); clr(progress_color); printf("%s", input); clr();
-									printf(". Игнорирование "); clr(progress_color); printf("{}"); clr(); printf(" отменено");
-									int NotAnUnsignedInt = input.length();
-									rd.seekg(-NotAnUnsignedInt,ios::cur);
-								}
-
-							}
-							else if (!rd.eof()) wr << symbol;
-				int cur = rd.tellg();
-				if (cur > 17915+2718-145) {
-					printf("");
 				}
+				else
+					if (symbol == '{') { //Р±Р»РѕРєРё
+						wr << symbol;
+						do {
+							symbol = rd.get();
+							wr << symbol;
+						} while (symbol != '}');  //С‡РёС‚Р°С‚СЊ, РїРѕРєР° Р±Р»РѕРє РЅРµ Р·Р°РєРѕРЅС‡РёС‚СЃСЏ
+					}
+					else
+						if (symbol == '@')
+						{
+							wr << symbol;
+							string input = "";
+							for (int i = 0; i < strlen("-moz-document"); i++)
+							{
+								input += rd.get();
+								if (input[i] == '/' || input[i] == '*' || input[i] == '{' || input[i] == ',' || input[i] == 13) {
+									rd.seekg(-i, ios::cur);
+									break;
+								}
+							}
+							if (input == "-moz-document") {
+								wr << input;
+								do {
+									symbol = rd.get();
+									wr << symbol;
+								} while (symbol != '{'); //РџРѕСЃР»Рµ -moz-document РёРіРЅРѕСЂРёСЂСѓРµС‚СЃСЏ РѕС‚РєСЂС‹С‚РёРµ Р±Р»РѕРєР°
+							}
+							else {
+								perr("Р’РЅРёРјР°РЅРёРµ: @ РѕР±РЅР°СЂСѓР¶РµРЅ, РѕР¶РёРґР°Р»РѕСЃСЊ "); clr(progress_color); printf("-moz-document");
+								clr(negative_color); printf("Р° РѕР±РЅР°СЂСѓР¶РµРЅРѕ "); clr(progress_color); printf("%s", input); clr();
+								printf(". РРіРЅРѕСЂРёСЂРѕРІР°РЅРёРµ "); clr(progress_color); printf("{}"); clr(); printf(" РѕС‚РјРµРЅРµРЅРѕ");
+								int NotAnUnsignedInt = input.length();
+								rd.seekg(-NotAnUnsignedInt, ios::cur);
+							}
+
+						}
+						else if (!rd.eof()) wr << symbol;
 			} while (!rd.eof());
 			wr.close();
-			clr(positive_color); printf("[dir]ирование кода завершено!");
-			clr(); printf(" Повторить?");
+			clr(positive_color); printf("[dir]РёСЂРѕРІР°РЅРёРµ РєРѕРґР° Р·Р°РІРµСЂС€РµРЅРѕ!");
+			clr(); printf(" РџРѕРІС‚РѕСЂРёС‚СЊ?");
 			Repeat = qu();
 		}
 	}
